4a10037b84dcfc06e957f27b8c8dcfdbb3c1a68f
Toufiquer <toufiquerabc@gmail.com>
2022-11-19 23:43:09 +0600

    clear is working

--- app.js
+++ app.js
@@ -1,6 +1,51 @@
+// Document Query Selector
 function dqs(id) {
   return document.querySelector(id);
 }
+function printText(id, value) {
+  dqs(id).innerText = value;
+}
+function outPutClear() {
+  dqs("#outPut").innerText = "";
+}
+function outPutTopClear() {
+  dqs("#outPut-top").innerText = "";
+}
+function forOperation(value, oldValue, oldValueTop, totalValue) {
+  if (oldValue === undefined && oldValueTop === undefined) {
+    return totalValue;
+  } else if (oldValue !== undefined) {
+     
+    return totalValue;
+  } else if (oldValueTop !== undefined) {
+    return totalValue;
+  }
+}
+// Any button Click in calculator
 dqs("#calculator-btn").addEventListener("click", function (e) {
-  console.log(e.target.innerText, " => Line No: 5");
+  const value = e.target.innerText;
+  let oldValue = dqs("#outPut").innerText;
+  let oldValueTop = dqs("#outPut-top").innerText;
+  let totalValue;
+
+  if (isNaN(value)) {
+    // Check for clear
+    if (value == "c") {
+      outPutClear();
+      outPutTopClear();
+    } else if (value == "+") {
+      totalValue = forOperation(value, oldValue, oldValueTop, totalValue);
+    } else if (value == "-") {
+    } else if (value == "*") {
+    } else if (value == "/") {
+    } else if (value == "=") {
+    }
+    console.log(" + ", value, " => Line No: 10");
+  } else {
+    if (oldValue !== "") {
+      printText("#outPut", oldValue + value);
+    } else {
+      printText("#outPut", value);
+    }
+  }
 });
